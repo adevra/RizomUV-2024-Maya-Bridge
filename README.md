849b4c2acf77f7ca18cfc96e0deced58a12f449c
adevran <33916683+adevra@users.noreply.github.com>
2025-02-01 23:49:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+![ssbridge.png](https://i.postimg.cc/YSXwpyTK/ssbridge.png)
+
+# RizomUV 2024 Bridge for Maya
+
+Initially started as an update for the Official Origami Digital Maya 2018 Bridge by Oliver Hotz, turned out to be a major re-write, The RizomUV 2024 Bridge for Maya provides a seamless workflow between Autodesk Maya and RizomUV, allowing users to easily transfer UV data for advanced UV unwrapping and packing. 
+
+
+## New Features
+
+- **Drag-and-Drop Installer**: Installer script will handle everything. 
+- **Namespace Handling**: Ensures that imported objects are correctly namespaced, avoiding multiple namespace entries upon each transfer.
+- **Material Assignment Preservation**: Maintains original material assignments after UV data is transferred back to Maya.
+- **Modern UI**: Script UI now matches the RizomUV 2024 colors
+
+
+## Installation
+
+1. **Download the Repository**: Download the repository from Github.
+2. **Choose the Correct Folder**: Navigate to the folder corresponding to your Maya Python version **(use py2 for Maya versions below 2022)**.
+3. **Drag-and-Drop Installer**: Locate the **drag_and_drop_install.py** script within the chosen folder.
+Drag and drop this file into the Maya viewport. Installer will prompt with a file browser to select RizomUV executable, (most likely inside C:\Program Files\Rizom Lab\...)
+it will then, add a shelf button under the currently active shelf. 
+
+## Usage
+
+Open the Script UI: 
+After installation, a new shelf button will be available in Maya. Click this button to open the RizomUV 2024 Bridge UI.
+
+Send Selected Geometry to RizomUV:
+Select meshes in Maya.
+In the RizomUV 2024 Bridge UI, click the **Send Selected** button.
+Optionally, check Transfer Existing UVs to Rizom if you want to include the existing UVs in the transfer.
+
+Get UVs from RizomUV:
+After editing UVs in RizomUV simply CTRL+S and save that file. 
+Go back to Maya viewport and select the meshes you want to replace their UVs and click the **Get UVs** button.
+Optionally, check Long Line Fix if you want to remove long lines from the imported UV data.
+
+Instant UVs:
+Use the Instant UVs button for an automated roundtrip workflow, sending the selected geometry to RizomUV, processing the UVs, and importing them back into Maya with one click.
+
+
+
+## Contributing
+Contributions are welcome! Please fork the repository and submit pull requests for any enhancements or bug fixes.
+
+## Support
+For any issues or questions, please open an issue on the GitHub repository.
---
+++ RizomUV Bridge 2024/drag_and_drop_install.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+import maya.cmds as cmds
+import maya.mel as mel
+import platform
+
+def onMayaDroppedPythonFile(*args):
+    try:
+        installer_directory = os.path.dirname(__file__)
+        script_file_path = os.path.join(installer_directory, "maya_rizomuv_bridge.py")
+        shelf_icon_path = os.path.join(installer_directory, "rzmuv.png")
+        
+        if not os.path.exists(script_file_path):
+            raise RuntimeError("Unable to find 'maya_rizomuv_bridge.py' relative to this installer")
+        if not os.path.exists(shelf_icon_path):
+            raise RuntimeError("Unable to find 'rzmuv.png' relative to this installer")
+        
+        if platform.system() == "Windows":
+            file_filter = "Executable Files (*.exe)"
+        elif platform.system() == "Darwin":
+            file_filter = "Applications (*.app)"
+        else:
+            file_filter = "All Files (*.*)"
+        
+        rizomPath = cmds.fileDialog2(fileMode=1, caption="Select RizomUV Executable", fileFilter=file_filter)[0]
+        
+        with open(script_file_path, 'r') as file:
+            script_content = file.read()
+
+        placeholder_paths = [
+            r"rizomPath = r'C:\\Program Files\\Rizom Lab\\RizomUV 2024.0\\rizomuv.exe'",
+            r"rizomPath = r'C:\Program Files\Rizom Lab\RizomUV 2024.0\rizomuv.exe'"
+        ]
+
+        for placeholder_path in placeholder_paths:
+            if placeholder_path in script_content:
+                script_content = script_content.replace(placeholder_path, f"rizomPath = r'{rizomPath}'")
+                break
+        else:
+            raise RuntimeError("Placeholder path not found in the script.")
+        
+        updated_script_file_path = os.path.join(installer_directory, "maya_rizomuv_bridge_updated.py")
+        with open(updated_script_file_path, 'w') as file:
+            file.write(script_content)
+        
+        prefs_dir = os.path.dirname(cmds.about(preferences=True))
+        scripts_dir = os.path.normpath(os.path.join(prefs_dir, "scripts"))
+        
+        shutil.copy(updated_script_file_path, os.path.join(scripts_dir, "maya_rizomuv_bridge.py"))
+        shutil.copy(shelf_icon_path, scripts_dir)
+
+        os.remove(updated_script_file_path)
+
+        absolute_shelf_icon_path = os.path.join(scripts_dir, "rzmuv.png").replace("\\", "/")
+
+        current_shelf = mel.eval("string $currentShelf = `tabLayout -query -selectTab $gShelfTopLevel`;")
+        cmds.setParent(current_shelf)
+        cmds.shelfButton(
+            enableCommandRepeat=True,
+            enable=True,
+            width=35,
+            height=34,
+            manage=True,
+            visible=True,
+            preventOverride=False,
+            annotation="RizomUV Bridge",
+            enableBackground=False,
+            backgroundColor=(0, 0, 0),
+            highlightColor=(0.321569, 0.521569, 0.65098),
+            align="center",
+            labelOffset=0,
+            rotation=0,
+            flipX=False,
+            flipY=False,
+            useAlpha=True,
+            overlayLabelColor=(1, 1, 1),
+            overlayLabelBackColor=(0, 0, 0, 0),
+            image=absolute_shelf_icon_path,
+            image1=absolute_shelf_icon_path,
+            style="iconOnly",
+            marginWidth=1,
+            marginHeight=1,
+            command="import maya_rizomuv_bridge; maya_rizomuv_bridge.createUI()"
+        )
+
+        cmds.confirmDialog(message="Script successfully installed to: {0}".format(scripts_dir),
+                           title="Confirmation dialog")
+    
+    except Exception as e:
+        cmds.confirmDialog(message="Script failed to install: {0}".format(e), icon="warning", title="ERROR")
+
+if __name__ == "__main__":
+    onMayaDroppedPythonFile()
